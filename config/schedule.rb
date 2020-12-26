2ba84bef9fe0fc3d20b75a61906b7d07ae76caf2
evazion <noizave@gmail.com>
2020-12-26 11:45:38 -0600

    cron: disable export_missing job.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,12 +1,12 @@
-env "MAILTO", "r888888888@gmail.com"
+env "MAILTO", "webmaster@danbooru.donmai.us"
 
 every 1.day do
   runner "HitCounter.new.prune!"
 end
 
-every 1.day do
-	runner "Archive::PostVersion.export_missing(48.hours.ago)"
-end
+#every 1.day do
+#	runner "Archive::PostVersion.export_missing(48.hours.ago)"
+#end
 
 every :sunday, :at => "1:00 am" do
   runner "BigQuery::Tag.new.export!"
