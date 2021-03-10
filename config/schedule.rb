764158edb38dd507fd9b9ad1c0f71ecc83366173
evazion <noizave@gmail.com>
2021-03-10 14:05:02 -0600

    Remove BigQuery export cron jobs.

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,18 +4,6 @@ every 1.day do
   runner "HitCounter.new.prune!"
 end
 
-#every 1.day do
-#	runner "Archive::PostVersion.export_missing(48.hours.ago)"
-#end
-
-every :sunday, :at => "1:00 am" do
-  runner "BigQuery::Tag.new.export!"
-end
-
-every :sunday, :at => "2:00 am" do
-  runner "BigQuery::User.new.export!"
-end
-
 every :sunday, :at => "3:00 am" do
   runner "Reports.generate_all"
 end
